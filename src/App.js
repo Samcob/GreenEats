27e020856b3c6080216b8153c6b82fe2e1b9957e
Sam O'Brien <samcob@uw.edu>
2019-02-24 20:47:10 -0800

    first of many submissions 02/24/18

--- src/App.js
+++ src/App.js
@@ -1,28 +1,81 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+//Bottom Navigation Bar imports.
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+
+//import Icon from "@material-ui/core/Icon"
+//Search Field imports.
+import Paper from '@material-ui/core/Paper';
+import InputBase from '@material-ui/core/InputBase';
+//import SearchIcon from '@material-ui/icons/Search';
+//import IconButton from '@material-ui/core/IconButton';
 import './App.css';
 
+const styles = {
+  root: {
+    display: "flex",
+  },
+  input: {
+    marginLeft: 8,
+    flex: 1,
+  },
+  searchBar: {
+    width: "auto",
+  },
+  iconButton: {
+    padding: 10,
+  },
+  wrapper: {
+    color: "#2D8B50",
+  },
+}
+
 class App extends Component {
+  state = {
+    value: 0,
+  };
+
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
   render() {
+    const { classes } = this.props;
+    const { value } = this.state;
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <a href="homepage.html"><img src="./Images/headerLogo.png" alt="Green Eats" height="45px"></img></a>
         </header>
-      </div>
+
+        <Paper className={classes.searchBar} elevation={1}>
+          <InputBase className={classes.input} placeholder="Search for a recipe " />
+          {/* <IconButton className={classes.iconButton} aria-label="Search">
+            <SearchIcon />
+          </IconButton> */}
+        </Paper>
+
+        <BottomNavigation className="bottom-nav">
+          value={value}
+          onChange={this.handleChange}
+          showLabels
+          className={classes.root}
+          >
+          <BottomNavigationAction label="Issues" value="issues" icon={"./Images / ecoTipsIcon.png"} />
+          <BottomNavigationAction label="EcoTips" value="ecotips" icon={"./Images/ecoTipsIcon.png"} />
+          <BottomNavigationAction label="Search" value="search" icon={"./Images/searchIcon.png"} />
+          <BottomNavigationAction label="EcoScore" value="ecoscore" icon={"./Images/ecoScoreIcon.png"} />
+        </BottomNavigation>
+      </div >
     );
   }
 }
 
-export default App;
+App.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(App);
