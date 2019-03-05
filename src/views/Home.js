18c8150c5616c2c2970c23d937c8cf85d43ea83b
Sam O'Brien <samcob@uw.edu>
2019-03-04 23:19:02 -0800

    pushing cleaned code that removed slogan animation

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,39 +1,13 @@
 import React, { Component } from 'react';
 
 import LargeResultCard from "../components/largeResultCard";
-// import Slogan from "../components/slogan";
-// import posed from "react-pose";
-import SplitText from "react-pose-text";
 
 import "../styles/home.css"
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-const charPoses = {
-    exit: {
-        opacity: 0,
-        x: "-100%"
-    },
-    enter: {
-        opacity: 1,
-        x: "0%",
-        beforeChildren: true,
-        staggerChildren: 50
-    }
-}
-
 class Home extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { modalVisible: false };
-    }
-
-    componentDidMount() {
-        setTimeout(() => {
-            this.setState({ modalVisible: true });
-        }, 1000);
-    }
 
     render() {
 
@@ -53,15 +27,9 @@ class Home extends Component {
                             </div>
                         </form>
 
-                        {/* <div initialPose="enter" pose="exit">
-                            <Slogan pose={this.state.modalVisible ? "enter" : "exit"} />
-                        </div>
-                        <div initialPose="enter" pose="exit"> */}
-
                         <p className="slogan">
-                            <SplitText charPoses={charPoses} pose={this.state.modalVisible ? "enter" : "exit"}>Sustainable eating made easy. Evaluate your meal's ecological impact.</SplitText>
+                            Sustainable eating made easy. Evaluate your meal's ecological impact.
                         </p>
-                        {/* </div> */}
 
                     </div>
 
