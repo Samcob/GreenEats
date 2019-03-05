204daabef2fffd0867ef1f9530483d42f39b8512
Your Name <you@example.com>
2019-03-04 17:39:57 -0800

    Fixed some errors

--- src/views/Results.js
+++ src/views/Results.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 
+// import "../styles/results.css";
+
 class Results extends Component {
     state = {}
     render() {
+        console.log("hi")
         return (
-            <div></div>
+            <div class="hi">hello</div>
         );
     }
 }
