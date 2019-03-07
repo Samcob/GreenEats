e7d66267810e34745e82c930fcfb3ae513dfa148
Your Name <you@example.com>
2019-03-06 23:20:52 -0800

    Finished Homepage

--- src/views/Home.js
+++ src/views/Home.js
@@ -39,26 +39,39 @@ class Home extends Component {
 
                 {/*Page Content*/}
 
-                <h2 class="ourPicks pl-1 mb-5"> Our Favorite Picks For Today!</h2>
+                {/* ./Images/PhotoBackgroundHorizontal.jpg */}
 
-                <Container>
-                    <Row>
-                        <Col className="columns" sm={12} md={6} lg={4}>
-                            <h5 class="pickTitles">Breakfast</h5>
-                            <LargeResultCard />
-                        </Col>
-                        <Col className="columns" sm={12} md={6} lg={4}>
-                            <h5 class="pickTitles">Lunch</h5>
-                            <LargeResultCard />
-                        </Col>
-                        <Col className="columns" sm={12} md={6} lg={4}>
-                            <h5 class="pickTitles">Dinner</h5>
-                            <LargeResultCard />
-                        </Col>
-                    </Row>
-                </Container>
+                <div class="suggestions">
 
-                <div class="footer"></div>
+
+                    <h2 class="ourPicks pl-1 mb-5"> Our Favorite Picks For Today!</h2>
+
+                    <Container fluid={true}>
+                        <Row>
+                            <Col className="columns" sm={12} md={6} lg={4}>
+                                <h5 class="pickTitles">Breakfast</h5>
+                                <LargeResultCard recipeTitle="Veggie Omelette" recipeImage="./Images/veggieOm.jpg"
+                                    ecoScore="5/5" time="30 Minutes" price="$$"
+                                    description="This omelette is so yummy, I make it all the time! It's loaded with healthy veggies." />
+                            </Col>
+                            <Col className="columns" sm={12} md={6} lg={4}>
+                                <h5 class="pickTitles">Lunch</h5>
+                                <LargeResultCard recipeTitle="Caprese Sandwhich" recipeImage="./Images/caprese.jpg"
+                                    ecoScore="5/5" time="15 Minutes" price="$"
+                                    description="In the summer, I usually make a Caprese salad with tomatoes, fresh mozzarella and grilled zucchini..." />
+                            </Col>
+                            <Col className="columns" sm={12} md={6} lg={4}>
+                                <h5 class="pickTitles">Dinner</h5>
+                                <LargeResultCard recipeTitle="Stir Fry" recipeImage="./Images/stirfry.jpg"
+                                    ecoScore="5/5" time="35 Minutes" price="$$"
+                                    description="This tangy stir fry is loaded with delious veggies. It’s so easy to make!" />
+                            </Col>
+                        </Row>
+                    </Container>
+
+                    <div class="footer"></div>
+
+                </div>
             </div >
         );
     }
