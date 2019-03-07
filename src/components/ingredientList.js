90bc20475cedc697222df69336978c6ae2f20c59
Sam O'Brien <samcob@uw.edu>
2019-03-06 21:02:53 -0800

    issues with creating a React component inside of a forEach loop

--- src/components/ingredientList.js
+++ src/components/ingredientList.js
@@ -2,10 +2,20 @@ import React, { Component } from 'react';
 import ListGroup from "react-bootstrap/ListGroup";
 import alertIcon from "../styles/Images/alertIcon.png";
 
+import Popup from "../components/popup.js";
+
 import "../styles/ingredientList.css";
 
 class IngredientList extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { modalShow: false };
+    }
+
     render() {
+        let modalClose = () => this.setState({ modalShow: false });
+
         return (
             <div>
                 {/*make into a for loop that generates list*/}
@@ -17,12 +27,24 @@ class IngredientList extends Component {
                     <ListGroup.Item>Kosher salt and freshly ground black pepper</ListGroup.Item>
                     <ListGroup.Item>2 cloves garlic, chopped</ListGroup.Item>
                     <ListGroup.Item>1 cup vegetable stock</ListGroup.Item>
-                    <ListGroup.Item variant="danger" action>1lb of Lamb
-                    <img src={alertIcon} alt="alert"></img></ListGroup.Item>
+
+                    <ListGroup.Item variant="danger" action
+                        onClick={() => this.setState({ modalShow: true })}
+                    >
+                        1lb of Lamb
+                    <img src={alertIcon} alt="alert"></img>
+                    </ListGroup.Item>
+
                     <ListGroup.Item>One cup of coconut milk</ListGroup.Item>
                     <ListGroup.Item>4 tablespoons curry spice</ListGroup.Item>
                 </ListGroup>
-            </div>
+
+                <Popup
+                    show={this.state.modalShow}
+                    onHide={modalClose}
+                />
+
+            </div >
         );
     }
 }
