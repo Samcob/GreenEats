eadf58eff7bc18c4519ff9c39eb9302918d47746
Sam O'Brien <samcob@uw.edu>
2019-03-06 22:35:59 -0800

    Still having issues with forEach

--- src/components/popup.js
+++ src/components/popup.js
@@ -6,21 +6,34 @@ import ListGroup from "react-bootstrap/ListGroup";
 class Popup extends Component {
     state = {}
 
+    // genList = (newIngredients) => {
+    //     return newIngredients.map(ingredient => (
+    //         <ListGroup.Item>
+    //             {ingredient}
+    //         </ ListGroup.Item>
+    //     ));
+    // }
+
     render() {
         let ingredient = "1lb of lamb";
         let bIngredient1 = "1 can of black beans";
         let bIngredient2 = "Tofu";
         var newIngredients = [{ bIngredient1 }, { bIngredient2 }];
+        var listItems = []
 
-        // function genList(ingredient) {
-        //     return (
-        //         <ListGroup.Item>
-        //             {ingredient}
-        //         </ListGroup.Item>
-        //     )
-        // }
+        function genList(newIngredients) {
+            newIngredients.forEach(function (ingredient) {
+                listItems.push(
+                    <ListGroup.Item>
+                        {ingredient}
+                    </ListGroup.Item>
+                )
+            })
+            return (listItems);
+        }
 
         return (
+
             <Modal
                 {...this.props}
                 size="md"
@@ -36,17 +49,8 @@ class Popup extends Component {
                     <h4 className="mb-3">Replace {ingredient} with:</h4>
 
                     <ListGroup variant="flush">
-                        {/* <ListGroup.Item>
-                            {bIngredient1}
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                            {bIngredient2}
-                        </ListGroup.Item> */}
+                        {genList(newIngredients)}
 
-                        {newIngredients.forEach(function genList(ingredient) {
-                            return genList(ingredient)
-                        })
-                        }
                     </ListGroup>
 
                     {/* <div className="clearOption">
@@ -55,16 +59,16 @@ class Popup extends Component {
                     </div>
                     <div className="divider">
                         <h6>Other options:</h6>
-
+                    
                     </div>
                     <div className="otherOption">
                         <Button variant="success">Save</Button>
-                    </div> */}
+                    </div>*/}
                 </Modal.Body>
                 <Modal.Footer>
                     <Button onClick={this.props.onHide}>Close</Button>
                 </Modal.Footer>
-            </Modal>
+            </Modal >
         );
     }
 }
