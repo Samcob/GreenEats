1859e3efdff06b73953283972e48d4522ef09471
Sam O'Brien <samcob@uw.edu>
2019-03-18 14:04:57 -0700

    Updates

--- precache-manifest.d1f2fa28b62454d0a596586efa97a06b.js
+++ precache-manifest.d1f2fa28b62454d0a596586efa97a06b.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "dfc5a5ca7467ab67da18",
-    "url": "/GreenEats/static/css/main.23b5722b.chunk.css"
+    "revision": "389f9621c0dc5afebda4",
+    "url": "/GreenEats/static/css/main.add7fc05.chunk.css"
   },
   {
-    "revision": "dfc5a5ca7467ab67da18",
-    "url": "/GreenEats/static/js/main.dfc5a5ca.chunk.js"
+    "revision": "389f9621c0dc5afebda4",
+    "url": "/GreenEats/static/js/main.389f9621.chunk.js"
   },
   {
     "revision": "9eaf084bd6de36bf9b1c",
@@ -20,12 +20,12 @@ self.__precacheManifest = [
     "url": "/GreenEats/static/js/2.be423af9.chunk.js"
   },
   {
-    "revision": "2ea26319e0f303309ea9484dcdc3a6c3",
-    "url": "/GreenEats/static/media/PhotoBackgroundVertical.2ea26319.jpg"
-  },
-  {
     "revision": "ca732b115dc8a7bf0c5b9543dadd13b5",
     "url": "/GreenEats/static/media/PhotoBackgroundHorizontal.ca732b11.jpg"
+  },
+  {
+    "revision": "2ea26319e0f303309ea9484dcdc3a6c3",
+    "url": "/GreenEats/static/media/PhotoBackgroundVertical.2ea26319.jpg"
   },
   {
     "revision": "acfd40c321e66d24463cf333c5a50c96",
@@ -68,7 +68,7 @@ self.__precacheManifest = [
     "url": "/GreenEats/static/media/back.c688b3d5.jpg"
   },
   {
-    "revision": "87e01d94065f76dd3b6bb842b6b8e37e",
+    "revision": "31a641c1ea98eee0fb3a6997775be795",
     "url": "/GreenEats/index.html"
   }
 ];
